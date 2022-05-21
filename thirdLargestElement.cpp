14d84c1eb66012116eb0b8e5de1e7a5763d0fea0
Gautam-2002 <gautam2sethi@gmail.com>
2022-05-21 16:26:08 +0530

    basic

---
+++ thirdLargestElement.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+        if(n<3){
+            return -1;
+        }
+        sort(a,a+n);
+        return a[n-3];
+    }
+    //without using sort();
+    // int thirdLargest(int a[], int n)
+    // {
+    //      //Your code here
+    //      if(n<3)    return -1;
+    //      int l1=a[0],l2=0,l3=-1;
+    //      for(int i=1;i<n;i++){
+    //          if(a[i]>l1){
+    //              l3=l2;
+    //              l2=l1;
+    //              l1=a[i];
+    //          }
+    //          else if(a[i]>l2){
+    //                  l3=l2;
+    //                  l2=a[i];
+    //              }
+    //          else if(a[i]>l3){
+    //              l3=a[i];
+    //          }
+    //      }
+    //      return l3;
+    // }
+
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}     // } Driver Code Ends
